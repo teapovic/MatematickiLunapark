b6fa2e374766becd1a200c96315747ee373dd348
Josipa Kelava <kelava.josipa@gmail.com>
2017-03-03 15:14:18 +0100

    navbari

--- app.js
+++ app.js
@@ -26,7 +26,9 @@ app.get('/', function (req, res) {
   res.render('home');
 });
 
-
+app.get("/tesko-oduzimanje", function (req, res) {
+    res.render("tesko_oduzimanje", {tema: "police"});
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
